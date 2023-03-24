e217eb10fd81b3f8b00a5fc94a5c01c32731c6d9
RACHANA AMIN <amipuria19@gmail.com>
2023-03-23 20:38:59 -0400

    commit

---
+++ bookstore/connectDB.php
@@ -0,0 +1,4 @@
+<?php
+// $pdo=new PDO('mysql:host=localhost;port=3306;dbname=id20169404_bookstore','root', '');
+$pdo=new PDO('mysql:host=localhost;port=3306;dbname=id20169404_bookstore','id20169404_root', '#K]l8V#djeDV&%M=');
+?>
